aaf1da4a40a5c1dc24d662a0db2bc2dcec9cf6e9
famstutz <florian@amstutz.nu>
2011-05-10 13:22:41 +0200

    chapter 10

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -2,7 +2,7 @@ class LineItemsController < ApplicationController
   # GET /line_items
   # GET /line_items.xml
   def index
-    @line_items = LineItem.all
+    @line_item = @cart.add_product(product.id)
 
     respond_to do |format|
       format.html # index.html.erb
@@ -46,8 +46,7 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to(@line_item.cart,
-          :notice => 'Line item was successfully created.') }
+        format.html { redirect_to(@line_item.cart) }
         format.xml  { render :xml => @line_item, :status => :created, :location => @line_item }
       else
         format.html { render :action => "new" }
