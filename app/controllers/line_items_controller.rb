5eefa35f4e9191bddccc67a2cea9998bcf392571
famstutz <florian@amstutz.nu>
2011-06-07 13:57:22 +0200

    fixed multiple products in cart

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -37,24 +37,26 @@ class LineItemsController < ApplicationController
     @line_item = LineItem.find(params[:id])
   end
 
-  # POST /line_items
+ # POST /line_items
   # POST /line_items.xml
-  def create
-    @cart = current_cart
-    product = Product.find(params[:product_id])
-    @line_item = @cart.line_items.build(:product => product)
+def create
+@cart = current_cart
+product = Product.find(params[:product_id])
+@line_item = @cart.add_product(product.id)
 
-    respond_to do |format|
-      if @line_item.save
-        format.html { redirect_to(store_url) }
-        format.js { @current_item = @line_item }
-        format.xml  { render :xml => @line_item, :status => :created, :location => @line_item }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @line_item.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
+respond_to do |format|
+if @line_item.save
+format.html { redirect_to(store_url) }
+format.js { @current_item = @line_item }
+format.xml { render :xml => @line_item,
+:status => :created, :location => @line_item }
+else
+format.html { render :action => "new" }
+format.xml { render :xml => @line_item.errors,
+:status => :unprocessable_entity }
+end
+end
+end
 
   # PUT /line_items/1
   # PUT /line_items/1.xml
